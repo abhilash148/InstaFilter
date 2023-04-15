1865d61bc9e552dc21171477c9ca4c698ac519d6
abhilash148 <saiabhilash148@gmail.com>
2023-04-15 03:55:59 -0500

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Sai Abhilash Gudavalli on 15/04/23.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
